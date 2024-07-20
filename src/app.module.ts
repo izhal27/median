7d544d6fa4c1856b130f4c19a96791d6f7e66aa3
Risal Walangadi <risal.walangadi@gmail.com>
2024-07-20 17:17:59 +0800

    add auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ArticlesModule, PrismaModule.forRoot({ isGlobal: true }), UsersModule,],
+  imports: [ArticlesModule, PrismaModule.forRoot({ isGlobal: true }), UsersModule, AuthModule,],
   controllers: [AppController],
   providers: [AppService],
 })
